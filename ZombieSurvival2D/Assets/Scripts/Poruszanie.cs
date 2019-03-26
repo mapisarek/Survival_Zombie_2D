105dc444cce7890ef0d2e3dc66f7f90cc7ca72c8
LeeZer0 <gotikwit@gmail.com>
2019-03-26 20:38:45 +0100

    dodanie grafiki

---
+++ ZombieSurvival2D/Assets/Scripts/Poruszanie.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poruszanie : MonoBehaviour
+{
+
+     
+    float speed = 2;
+    private Rigidbody2D rig;
+    public Animator m_Animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, vAxis, vAxis) * speed * Time.deltaTime;
+        rig.MovePosition(transform.position+movement);
+
+
+        m_Animator.SetFloat("Poziom", hAxis);
+        m_Animator.SetFloat("Pion", vAxis);
+
+
+
+    }
+}
