d6f73023c93746f23bca50bf7b81c92895c2a2da
mapisarek <marekpisarek94@gmail.com>
2019-03-30 16:37:32 +0100

    Task 2: Scenes have been edited, scripts and loading of individual scenes have been added

---
+++ ZombieSurvival2D/Assets/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    
+    public void LoadInstructions()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ ZombieSurvival2D/Assets/SettingsMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void BackButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+}
