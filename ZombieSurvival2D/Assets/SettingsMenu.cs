11b66fc3497695d213ecbf0df2da571bd611a997
mapisarek <marekpisarek94@gmail.com>
2019-03-31 21:36:07 +0200

    Task 2: Added options in settings scene

--- ZombieSurvival2D/Assets/SettingsMenu.cs
+++ ZombieSurvival2D/Assets/SettingsMenu.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    private CanvasGroup SettingCanvas;
+    public Canvas SettingsCanvas;
+
     public void BackButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    public void LoadVolumeSettings()
+    {
+        gameObject.transform.Find("SettingCanvas").gameObject.SetActive(true);
     }
 }
