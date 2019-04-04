b17054954c1882dda0c5fa53560607561eaa83f3
Dominik1534 <dominikkosatka@Gmail.comHubert1534>
2019-04-04 13:56:39 +0200

    Create class to represent the tiles in the world.

---
+++ ZombieSurvival2D/Assets/Scripts/Classes/MeshData.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MeshData
+{
+   public List<Vector3> vertices;
+    public List<int> triangles;
+    public MeshData(Tile[,]data)
+    {
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+
+        for (int i = 0; i < data.GetLength(0); i++)
+        {
+            for (int j = 0; j < data.GetLength(1); j++)
+            {
+                CreateSquare(data[i, j], i, j);
+            }
+        }
+    }
+    void CreateSquare(Tile tile,int x, int y)
+    {
+        vertices.Add(new Vector3(x + 0, y + 0));
+        vertices.Add(new Vector3(x + 1, y + 0));
+        vertices.Add(new Vector3(x + 0, y + 1));
+        vertices.Add(new Vector3(x + 1, y + 1));
+
+        triangles.Add(vertices.Count - 1);
+        triangles.Add(vertices.Count - 3);
+        triangles.Add(vertices.Count - 4);
+
+
+        triangles.Add(vertices.Count - 2);
+        triangles.Add(vertices.Count - 1);
+        triangles.Add(vertices.Count - 4);
+
+    }
+
+}
+
