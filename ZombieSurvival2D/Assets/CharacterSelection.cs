072503f743cd7d9e05da7c508f3cfc9b20a873fd
mapisarek <marekpisarek94@gmail.com>
2019-04-06 19:25:28 +0200

    Task 2: Added class with start method to control the user choice

---
+++ ZombieSurvival2D/Assets/CharacterSelection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+    private GameObject[] characterList;
+
+    private void Start()
+    {
+        characterList = new GameObject[transform.childCount];
+        
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            characterList[i] = transform.GetChild(i).gameObject;
+        }
+
+        foreach(GameObject go in characterList)
+        {
+            go.SetActive(false);
+        }
+
+        if (characterList[0])
+        {
+            characterList[0].SetActive(true);
+        }
+    }
+}
+
