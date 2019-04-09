de8669cf24930475ece68c56f0d44494b302e8c2
mapisarek <marekpisarek94@gmail.com>
2019-04-09 21:00:02 +0200

    Task 12: Rescaling the menu

--- ZombieSurvival2D/Assets/Inventory.cs
+++ ZombieSurvival2D/Assets/Inventory.cs
@@ -7,15 +7,30 @@ public class Inventory : MonoBehaviour
 
     private bool inventoryEnabled;
     public GameObject inventory;
+    public GameObject equipment;
 
-    private int allSlots;
-    private int enablesSlots;
-    private GameObject[] slot;
+    private int allInventorySlots;
+    private int enablesInventorySlots;
+    private int allEquipmentSlots;
+    private int enablesEquipmentSlots;
 
-    public GameObject slotHolder;
+    private GameObject[] inventorySlot;
+    private GameObject[] equipmentSlot;
+
+    public GameObject inventoryHolder;
+    public GameObject equipmentHolder;
 
     void Start()
     {
+        allInventorySlots = 104;
+        allEquipmentSlots = 5;
+
+        inventorySlot = new GameObject[allInventorySlots];
+
+        for(int i=0; i < allInventorySlots; i++)
+        {
+            inventorySlot[i] = inventoryHolder.transform.GetChild(i).gameObject;
+        }
 
     }
 
@@ -29,10 +44,12 @@ public class Inventory : MonoBehaviour
         if(inventoryEnabled == true)
         {
             inventory.SetActive(true);
+            equipment.SetActive(true);
         }
         else
         {
             inventory.SetActive(false);
+            equipment.SetActive(false);
         }
     }
 }
