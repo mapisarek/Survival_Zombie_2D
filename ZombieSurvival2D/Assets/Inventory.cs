66ad50c1e1f6f2e3eba54bc3ed35e533da507702
mapisarek <marekpisarek94@gmail.com>
2019-04-09 19:24:13 +0200

    Task 20: Added class that will control the user interface

---
+++ ZombieSurvival2D/Assets/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public bool inventoryEnabled;
+    public GameObject inventory;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryEnabled = !inventoryEnabled;
+        }
+
+        if(inventoryEnabled == true)
+        {
+            inventory.SetActive(true);
+        }
+        else
+        {
+            inventory.SetActive(false);
+        }
+    }
+}
