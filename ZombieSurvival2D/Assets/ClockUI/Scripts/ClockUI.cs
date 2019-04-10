8f4bd775d6962db4338c1a362b599381ee9745f8
mapisarek <marekpisarek94@gmail.com>
2019-04-10 20:22:12 +0200

    Task 12: Added clock with necessary textures

---
+++ ZombieSurvival2D/Assets/ClockUI/Scripts/ClockUI.cs
@@ -0,0 +1,52 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockUI : MonoBehaviour {
+
+    private const float REAL_SECONDS_PER_INGAME_DAY = 60f;
+
+    private Transform clockHourHandTransform;
+    private Transform clockMinuteHandTransform;
+    private Text timeText;
+    private float day;
+
+    private void Awake() {
+        clockHourHandTransform = transform.Find("hourHand");
+        clockMinuteHandTransform = transform.Find("minuteHand");
+        timeText = transform.Find("timeText").GetComponent<Text>();
+    }
+
+    private void Update() {
+        day += Time.deltaTime / REAL_SECONDS_PER_INGAME_DAY;
+
+        float dayNormalized = day % 1f;
+
+        float rotationDegreesPerDay = 360f;
+        clockHourHandTransform.eulerAngles = new Vector3(0, 0, -dayNormalized * rotationDegreesPerDay);
+
+        float hoursPerDay = 24f;
+        clockMinuteHandTransform.eulerAngles = new Vector3(0, 0, -dayNormalized * rotationDegreesPerDay * hoursPerDay);
+
+        string hoursString = Mathf.Floor(dayNormalized * hoursPerDay).ToString("00");
+
+        float minutesPerHour = 60f;
+        string minutesString = Mathf.Floor(((dayNormalized * hoursPerDay) % 1f) * minutesPerHour).ToString("00");
+
+        timeText.text = hoursString + ":" + minutesString;
+    }
+
+}
