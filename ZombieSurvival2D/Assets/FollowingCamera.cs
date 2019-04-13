5d03a9253e81362acff8968c14c15c62e28b2fd2
mapisarek <marekpisarek94@gmail.com>
2019-04-13 02:28:06 +0200

    Task 18: Added properties with methods to the class

--- ZombieSurvival2D/Assets/FollowingCamera.cs
+++ ZombieSurvival2D/Assets/FollowingCamera.cs
@@ -5,6 +5,18 @@ using UnityEngine;
 
 public class FollowingCamera : MonoBehaviour
 {
+    public GameObject characterList;
+    private int index;
+    private CinemachineVirtualCamera camFollower;
 
+    public void Start()
+    {
+
+    }
+
+    private void SetUpCamera()
+    {
+
+    }
 
 }
