46184cc46dfac7a489591aee71fded57fe1c0a69
LeeZer0 <gotikwit@gmail.com>
2019-04-17 19:20:10 +0200

    task 25: initialization of variables

--- ZombieSurvival2D/Assets/Scripts/WeaponShot.cs
+++ ZombieSurvival2D/Assets/Scripts/WeaponShot.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class WeaponShot : MonoBehaviour
 {
+
+    private Rigidbody2D rig;
+    private Vector2 spherePosition;
+    public GameObject prefab;
+    private float canShot = 0.0f;
+    private float canAmo = 0.0f;
+    public int amo = 300;
+    public int magazin = 3;
+    private int rel = 0;
+    public int damage = 1;
+    public bool isEnemyShot = false;
+    private float barWidth;
+    private float barHeight;
     // Start is called before the first frame update
     void Start()
     {
