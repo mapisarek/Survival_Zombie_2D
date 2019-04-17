e53b8113e13a5a232ea4889f915f58d3635c3795
mapisarek <marekpisarek94@gmail.com>
2019-04-17 16:52:57 +0200

    Task 9: Added properties to the class that will be used for generate Monsters

--- ZombieSurvival2D/Assets/MobSpawner.cs
+++ ZombieSurvival2D/Assets/MobSpawner.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class MobSpawner : MonoBehaviour
 {
+    public GameObject[] animals;
 
+    [Space(3)]
+    public float waitingForNextSpawn = 10;
+    public float theCountdown = 10;
+    
+    [Header("X Spawn Range")]
+    public float xMin;
+    public float xMax;
+
+    [Header("Y Spawn Range")]
+    public float yMin;
+    public float yMax;
+
+
+    
 }
