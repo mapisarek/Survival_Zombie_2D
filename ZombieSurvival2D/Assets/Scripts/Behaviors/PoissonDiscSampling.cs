e3863ab0e9ad2aa7640cb0800d3857f92bec4e3a
Dominik1534 <dominikkosatka@Gmail.comHubert1534>
2019-04-18 17:47:16 +0200

    Task 21: Finisz Poisson Disc Sampling Algorithm.

--- ZombieSurvival2D/Assets/Scripts/Behaviors/PoissonDiscSampling.cs
+++ ZombieSurvival2D/Assets/Scripts/Behaviors/PoissonDiscSampling.cs
@@ -42,4 +42,34 @@ public class NewBehaviourScript : MonoBehaviour
 
         return points;
     }
+    static bool IsValid(Vector2 candidate, Vector2 sampleRegionSize, float cellSize, float radius, List<Vector2> points, int[,] grid)
+    {
+        if (candidate.x >= 0 && candidate.x < sampleRegionSize.x && candidate.y >= 0 && candidate.y < sampleRegionSize.y)
+        {
+            int cellX = (int)(candidate.x / cellSize);
+            int cellY = (int)(candidate.y / cellSize);
+            int searchStartX = Mathf.Max(0, cellX - 2);
+            int searchEndX = Mathf.Min(cellX + 2, grid.GetLength(0) - 1);
+            int searchStartY = Mathf.Max(0, cellY - 2);
+            int searchEndY = Mathf.Min(cellY + 2, grid.GetLength(1) - 1);
+
+            for (int x = searchStartX; x <= searchEndX; x++)
+            {
+                for (int y = searchStartY; y <= searchEndY; y++)
+                {
+                    int pointIndex = grid[x, y] - 1;
+                    if (pointIndex != -1)
+                    {
+                        float sqrDst = (candidate - points[pointIndex]).sqrMagnitude;
+                        if (sqrDst < radius * radius)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 }
