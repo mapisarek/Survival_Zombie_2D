cd4cbdac301dc286f9421275cfaf4073cf999df6
mapisarek <marekpisarek94@gmail.com>
2019-04-18 22:25:59 +0200

    Task 9: Added field Target capture in class

--- ZombieSurvival2D/Assets/Scripts/Enemy.cs
+++ ZombieSurvival2D/Assets/Scripts/Enemy.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private Transform target;
+
+    public Transform Target
     {
-        
+        get
+        {
+            return target;
+        }
+
+        set
+        {
+            target = value;
+        }
     }
 }
