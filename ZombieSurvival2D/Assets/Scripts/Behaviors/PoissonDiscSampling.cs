26f385facf8b89f8aed1274a312ef2ead09a8ae8
Dominik1534 <dominikkosatka@Gmail.comHubert1534>
2019-04-18 17:40:57 +0200

    Task 21: Create new Script which generate points.

---
+++ ZombieSurvival2D/Assets/Scripts/Behaviors/PoissonDiscSampling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public static List<Vector2> GeneratePoints(float radius, Vector2 sampleRegionSize, int numSamplesBeforeRejection = 30)
+    {
+        float cellSize = radius / Mathf.Sqrt(2);
+
+        int[,] grid = new int[Mathf.CeilToInt(sampleRegionSize.x / cellSize), Mathf.CeilToInt(sampleRegionSize.y / cellSize)];
+        List<Vector2> points = new List<Vector2>();
+        List<Vector2> spawnPoints = new List<Vector2>();
+
+        spawnPoints.Add(sampleRegionSize / 2);
+        while (spawnPoints.Count > 0)
+        {
+            int spawnIndex = Random.Range(0, spawnPoints.Count);
+            Vector2 spawnCentre = spawnPoints[spawnIndex];
+            bool candidateAccepted = false;
+
+            
+
+        }
+
+        return points;
+    }
+}
