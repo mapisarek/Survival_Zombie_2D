20e97e656592773c669901cbedcca446eda70ad5
Dominik1534 <dominikkosatka@Gmail.comHubert1534>
2019-04-18 17:50:40 +0200

    Task 21: Create Tree Spawner scrippt.

---
+++ ZombieSurvival2D/Assets/Scripts/Behaviors/TreeSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour
+{
+
+    public float radius = 1;
+    public Vector2 regionSize = Vector2.one;
+    public int rejectionSamples = 30;
+
+
+    public GameObject tree;
+    List<Vector2> points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
