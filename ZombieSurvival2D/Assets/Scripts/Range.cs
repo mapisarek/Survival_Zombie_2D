1994e87a7de00cac5dc9c5b305298ae1e46a9a17
mapisarek <marekpisarek94@gmail.com>
2019-04-18 22:19:29 +0200

    Task 9: Added start method that gets parentComponent

--- ZombieSurvival2D/Assets/Scripts/Range.cs
+++ ZombieSurvival2D/Assets/Scripts/Range.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Range : MonoBehaviour
 {
+    private Enemy parent;
+
+    private void Start()
+    {
+        parent = GetComponentInParent<Enemy>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
