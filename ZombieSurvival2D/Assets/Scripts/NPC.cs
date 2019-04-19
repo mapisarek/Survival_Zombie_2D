6dfead762ea0ce0bd514c060e34bee5ebf183b99
mapisarek <marekpisarek94@gmail.com>
2019-04-20 01:08:55 +0200

    Task 9: Added abstract character class

--- ZombieSurvival2D/Assets/Scripts/NPC.cs
+++ ZombieSurvival2D/Assets/Scripts/NPC.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
+    [SerializeField]
+    private float speed;
+    private Vector2 direction;
+
     void Start()
     {
         
@@ -16,6 +20,30 @@ public class NPC : MonoBehaviour
 
     public void Movement()
     {
-        transform.Translate();
+        transform.Translate(direction * speed * Time.deltaTime);
     }
+
+    /*
+    private void InputKeys()
+    {
+        direction = Vector2.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down; 
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.left;
+        }
+    }
+    */
 }
