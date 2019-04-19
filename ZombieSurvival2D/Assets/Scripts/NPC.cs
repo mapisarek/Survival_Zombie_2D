d9c17483dba8e5941a07885e502621fce63c7aaa
mapisarek <marekpisarek94@gmail.com>
2019-04-20 00:58:31 +0200

    Task 9: Added NPC class to man created objects

---
+++ ZombieSurvival2D/Assets/Scripts/NPC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Movement()
+    {
+        
+    }
+}
