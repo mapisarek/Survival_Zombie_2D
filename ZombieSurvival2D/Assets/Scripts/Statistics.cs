759e083b199ed0207e9fa06ddeef9a63f4807f03
mapisarek <marekpisarek94@gmail.com>
2019-04-21 00:20:42 +0200

    Task 27: Added used properties to the Statistics class

--- ZombieSurvival2D/Assets/Scripts/Statistics.cs
+++ ZombieSurvival2D/Assets/Scripts/Statistics.cs
@@ -1,8 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Statistics : MonoBehaviour
 {
+    private Image content;
+    private float currentFill;
+    public float InitMaxValue { get; set; }
 
+    public float CurrentValue
+    {
+        get
+        {
+            return currentValue;
+        }
+
+        set
+        {
+            currentValue = value;
+        }
+    }
+
+    private float currentValue;
+
+    private void Start()
+    {
+        content = GetComponent<Image>();
+    }
 }
