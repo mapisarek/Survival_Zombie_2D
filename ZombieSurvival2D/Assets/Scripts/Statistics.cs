9337b3490e713f57c4425b15c0ab145d9b99eb2d
mapisarek <marekpisarek94@gmail.com>
2019-04-21 00:40:56 +0200

    Task 27: Added Update function with MathLerp to smooth bar resize

--- ZombieSurvival2D/Assets/Scripts/Statistics.cs
+++ ZombieSurvival2D/Assets/Scripts/Statistics.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class Statistics : MonoBehaviour
 {
+    [SerializeField]
+    private float lerpSpeed;
     private Image content;
     private float currentFill;
     public float InitMaxValue { get; set; }
@@ -45,5 +47,13 @@ public class Statistics : MonoBehaviour
     {
         InitMaxValue = maxValue;
         CurrentValue = currentValue;
+    }
+
+    public void Update()
+    {
+        if (currentFill != content.fillAmount)
+        {
+            content.fillAmount = Mathf.Lerp(content.fillAmount, currentFill, Time.deltaTime * lerpSpeed);
+        }
     }
 }
