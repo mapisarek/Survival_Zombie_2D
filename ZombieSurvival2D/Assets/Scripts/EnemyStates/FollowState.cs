bc9d035fa117ab4f9c58b09daa6612f2b48f9050
mapisarek <marekpisarek94@gmail.com>
2019-04-22 00:12:12 +0200

    Task 27: Added FollowState : IState

---
+++ ZombieSurvival2D/Assets/Scripts/EnemyStates/FollowState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class FollowState : IState
+{
+    private Enemy parent;
+    public void Enter(Enemy parent)
+    {
+        this.parent = parent;
+    }
+
+    public void Exit()
+    {
+
+    }
+
+    public void Update()
+    {
+    }
+}
