68f705d1fbf65aebcec03ad2532d2f4c80e08a8f
mapisarek <marekpisarek94@gmail.com>
2019-04-24 17:39:56 +0200

    Task 27: Added another state to the State Machine pattern

---
+++ ZombieSurvival2D/Assets/Scripts/EnemyStates/EvadeState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvadeState : IState
+{
+    private Enemy parent;
+
+    public void Enter(Enemy parent)
+    {
+        this.parent = parent;
+    }
+
+    public void Exit()
+    {
+        parent.Direction = Vector2.zero;
+    }
+
+    public void Update()
+    {
+    }
+}
