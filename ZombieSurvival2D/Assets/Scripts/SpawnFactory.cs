7b1cbae27fffeb0fa2e859e3d52b3eb3799a0a0b
mapisarek <marekpisarek94@gmail.com>
2019-05-05 00:34:43 +0200

    Task 27: Added interface to the FactoryPattern

--- ZombieSurvival2D/Assets/Scripts/SpawnFactory.cs
+++ ZombieSurvival2D/Assets/Scripts/SpawnFactory.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 namespace Assets.Scripts
 {
-    
+    interface ISpawnFactory
+    {
+        void SpawnEnemy(GameObject[] enemy, float xMin, float xMax,
+            float yMin, float yMax);
+    }
+
+   
 
 }
