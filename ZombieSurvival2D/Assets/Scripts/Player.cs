b05d46e35571b490b848fe73f064c7f74a74cbed
mapisarek <marekpisarek94@gmail.com>
2019-05-04 15:28:33 +0200

    Task 27: Added running ability to the refactored script

--- ZombieSurvival2D/Assets/Scripts/Player.cs
+++ ZombieSurvival2D/Assets/Scripts/Player.cs
@@ -24,17 +24,42 @@ public class Player : Character
     private float armorValue;
     [SerializeField]
     private float maxArmor;
+    private float basicSpeed;
+    private float runSpeed;
+	private float Timer;
+	private bool isRunning;
 
+
+    [SerializeField]
+    private float boost;
+    [SerializeField]
+    private float staminaWaste;
+    [SerializeField]
+    private float staminaRegeneration;
+  
+	
     protected override void Start()
     {
-      //  health.Initialize(healthValue,maxHealth);
+        //  health.Initialize(healthValue,maxHealth);
         //stamina.Initialize(staminaValue, maxStamina);
         //armor.Initialize(armorValue, maxArmor);
+        setupVariables();
         base.Start();
+    }
+
+    private void setupVariables()
+    {
+        staminaRegeneration = 2;
+        staminaWaste = 15;
+        boost = 2;
+        basicSpeed = Speed;
+        runSpeed = basicSpeed * boost;
     }
 
     protected override void Update()
     {
+		Timer += Time.deltaTime;
+		basicSetup();
         InputKeys();
         //health.CurrentValue = 100;
         base.Update();
@@ -60,6 +85,45 @@ public class Player : Character
         {
             Direction += Vector2.right;
         }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if(staminaValue > 0)
+			isRunning = true;
+            if (staminaValue < 0)
+                isRunning = false;
+        }
+		else{
+			isRunning = false;
+		}
+        
     }
-    
+	
+	private void basicSetup(){
+		if(!isRunning){
+			Speed = basicSpeed;
+			staminaRegen();
+		}
+		if(isRunning){
+		Speed = runSpeed;
+        if(IsMoving)
+        burnStamina();
+		}
+	}
+	
+	private void burnStamina(){
+	 if(staminaValue > 0)
+            {
+            staminaValue -= Time.deltaTime * staminaWaste;
+            Timer = 0;
+			}
+	}
+	
+    public void staminaRegen()
+    {
+        if(Timer > 5 && staminaValue <= maxStamina)
+        {
+            staminaValue += Time.deltaTime * staminaRegeneration;
+        }
+    }
+		
 }
