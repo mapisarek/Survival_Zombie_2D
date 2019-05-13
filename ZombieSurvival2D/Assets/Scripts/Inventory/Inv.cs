cb7a4bc16c9b6d0172d1d8e789c513c134756f2f
mapisarek <marekpisarek94@gmail.com>
2019-05-13 17:44:47 +0200

    Task 24: Added singleton pattern and inventory properties

--- ZombieSurvival2D/Assets/Scripts/Inventory/Inv.cs
+++ ZombieSurvival2D/Assets/Scripts/Inventory/Inv.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inv : MonoBehaviour
+{
+    #region Singleton
+    public static Inv instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("Inventory existing");
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    public List<Items> items = new List<Items>();
+    public int space = 20;
+
+}
