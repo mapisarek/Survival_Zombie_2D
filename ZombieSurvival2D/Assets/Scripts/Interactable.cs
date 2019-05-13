994a3da9aeee75138ccdaee93d96668f4f5d3e84
mapisarek <marekpisarek94@gmail.com>
2019-05-13 18:00:15 +0200

    Task 24: Added OnDraw Gizmos effect

--- ZombieSurvival2D/Assets/Scripts/Interactable.cs
+++ ZombieSurvival2D/Assets/Scripts/Interactable.cs
@@ -33,5 +33,15 @@ public class Interactable : MonoBehaviour
 
 
 
+    private void OnDrawGizmosSelected()
+    {
+        if (interactionTransform == null)
+            interactionTransform = transform;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position,radius);
+    }
+
+  
 
 }
