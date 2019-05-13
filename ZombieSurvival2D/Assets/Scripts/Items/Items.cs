5cb6033df72f67e14e18735ba9bbb1e9df626d7d
mapisarek <marekpisarek94@gmail.com>
2019-05-13 17:56:35 +0200

    Task 24: Added scriptable object with basic properties

--- ZombieSurvival2D/Assets/Scripts/Items/Items.cs
+++ ZombieSurvival2D/Assets/Scripts/Items/Items.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Item", menuName ="Inventory/Item")]
+
+public class Items : ScriptableObject
+{
+    new public string name = "New item";
+    public Sprite icon = null;
+    public bool isDefaultItem = false;
+
+
+}
