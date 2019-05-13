c566e32f88ea6ababe25815f1536cb816120d4ac
mapisarek <marekpisarek94@gmail.com>
2019-05-13 17:50:02 +0200

    Task 24: Added updateUI method

--- ZombieSurvival2D/Assets/Scripts/Inventory/InvUI.cs
+++ ZombieSurvival2D/Assets/Scripts/Inventory/InvUI.cs
@@ -15,6 +15,24 @@ public class InvUI : MonoBehaviour
 
         slots = itemsParent.GetComponentsInChildren<InventorySlot>();
     }
+void Update()
+    {
+        
+    }
 
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventory.items.Count)
+            {
+                slots[i].AddItem(inventory.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
     
 }
