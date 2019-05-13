d18e05bd4654b23db0de4443ef986d469b112e01
mapisarek <marekpisarek94@gmail.com>
2019-05-13 17:51:37 +0200

    Task 24: Added properties to inventory slot class

--- ZombieSurvival2D/Assets/Scripts/Inventory/InventorySlot.cs
+++ ZombieSurvival2D/Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon;
+    public Button removeButton;
+
+    Items item;
+
+  
+
+}
