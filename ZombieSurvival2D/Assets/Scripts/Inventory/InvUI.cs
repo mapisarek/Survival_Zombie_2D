d65914567cd748df790ca98a8b0a51f201556c78
mapisarek <marekpisarek94@gmail.com>
2019-05-13 17:47:35 +0200

    Task 24: Added properties to inventory UI class

--- ZombieSurvival2D/Assets/Scripts/Inventory/InvUI.cs
+++ ZombieSurvival2D/Assets/Scripts/Inventory/InvUI.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvUI : MonoBehaviour
+{
+    public Transform itemsParent;
+    Inv inventory;
+    InventorySlot[] slots;
+
+   
+}
