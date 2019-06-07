1cd71a7c80f221882999dd565dfba9b7ca490f82
mapisarek <marekpisarek94@gmail.com>
2019-06-07 14:29:05 +0200

    Task 25: Added OnDraw method in attack script

--- ZombieSurvival2D/Assets/Scripts/PlayerAttack.cs
+++ ZombieSurvival2D/Assets/Scripts/PlayerAttack.cs
@@ -16,4 +16,10 @@ public class PlayerAttack : MonoBehaviour
         
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+
 }
