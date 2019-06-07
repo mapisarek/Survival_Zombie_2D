ad3ebf632111f0ef9dd8368d86172aa1410f525b
mapisarek <marekpisarek94@gmail.com>
2019-06-07 14:12:47 +0200

    Fixed untracked files

--- ZombieSurvival2D/Assets/Scripts/Attack.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Attack : MonoBehaviour
-{
-    public int damage = 1;
-    public bool isEnemyShot = false;
-}
