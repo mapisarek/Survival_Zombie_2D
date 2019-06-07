76707b1081177b87a61843f23fcff03872fe4454
mapisarek <marekpisarek94@gmail.com>
2019-06-07 14:30:07 +0200

    Task 25: Added Attack in range method

--- ZombieSurvival2D/Assets/Scripts/PlayerAttack.cs
+++ ZombieSurvival2D/Assets/Scripts/PlayerAttack.cs
@@ -22,4 +22,25 @@ public class PlayerAttack : MonoBehaviour
         Gizmos.DrawWireSphere(attackPos.position, attackRange);
     }
 
+    private void attackInRange()
+    {
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+
+            timeBtwAttack = startTimeBtwAttack;
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+    }
+    
 }
