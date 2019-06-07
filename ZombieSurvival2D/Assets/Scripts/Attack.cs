a3baa783cef1608bf69b4007889c430b6bba2df0
mapisarek <marekpisarek94@gmail.com>
2019-06-07 19:54:36 +0200

    Task 30: Added attack class

--- ZombieSurvival2D/Assets/Scripts/Attack.cs
+++ ZombieSurvival2D/Assets/Scripts/Attack.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    public int damage = 1;
-    public bool isEnemyShot = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
