3fabc870cc9e5e174a83f97d2406e8989e92c5d9
mapisarek <marekpisarek94@gmail.com>
2019-06-07 14:28:38 +0200

    Task 25: Added properties to attack script

--- ZombieSurvival2D/Assets/Scripts/PlayerAttack.cs
+++ ZombieSurvival2D/Assets/Scripts/PlayerAttack.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+    public Transform attackPos;
+    public float attackRange;
+    public LayerMask whatIsEnemies;
+    public int damage;
 
     void Update()
     {
