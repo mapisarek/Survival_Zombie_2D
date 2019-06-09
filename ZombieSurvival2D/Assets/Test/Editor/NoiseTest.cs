802d501a72407fb532af119641738d7314ba8665
Dominik1534 <dominikkosatka@Gmail.comHubert1534>
2019-06-09 18:28:50 +0200

    Task 31: Create body of test.

--- ZombieSurvival2D/Assets/Test/Editor/NoiseTest.cs
+++ ZombieSurvival2D/Assets/Test/Editor/NoiseTest.cs
@@ -8,7 +8,14 @@ public class NoiseTest
     
     public void SeedIsGreatherOrEqualThanZero(int seed, float frequency, float amplitude, float lecunarity, float presistance, int octawes)
     {
-        
+        var n = new Noise(seed, frequency, amplitude, lecunarity, presistance, octawes);
+
+        Assert.GreaterOrEqual(seed, 0);
+        Assert.GreaterOrEqual(frequency, 0);
+        Assert.GreaterOrEqual(amplitude, 0);
+        Assert.GreaterOrEqual(lecunarity, 0);
+        Assert.GreaterOrEqual(presistance, 0);
+        Assert.GreaterOrEqual(octawes, 0);
     }
 
 
