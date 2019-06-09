511cd70c19dad1a7a39e4b50001f9c15771b86bf
Dominik1534 <dominikkosatka@Gmail.comHubert1534>
2019-06-09 18:24:10 +0200

    Task 31: Create NoiseTestClass.

---
+++ ZombieSurvival2D/Assets/Test/Editor/NoiseTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class NoiseTest
+{
+    
+    public void SeedIsGreatherOrEqualThanZero(int seed, float frequency, float amplitude, float lecunarity, float presistance, int octawes)
+    {
+        
+    }
+
+
+    
+
+}
