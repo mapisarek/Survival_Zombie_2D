a3ff70242f79f342128c17469ca565394c73b315
Dominik1534 <dominikkosatka@Gmail.comHubert1534>
2019-06-09 18:37:20 +0200

    Task 31: Additional variants of test SeedIsGreatherOrEqualThanZero.

--- ZombieSurvival2D/Assets/Test/Editor/NoiseTest.cs
+++ ZombieSurvival2D/Assets/Test/Editor/NoiseTest.cs
@@ -5,7 +5,12 @@ using NUnit.Framework;
 
 public class NoiseTest
 {
-    
+    [TestCase(1, 1, 1, 1, 1, 1)]
+    [TestCase(0, 0, 2, 1, 1, 1)]
+    [TestCase(0, 0, 0, 0, 0, 0)]
+    [TestCase(100, 5, 0, 0, 0, 0)]
+    [TestCase(1, 8, 5, 0.12, 0.69, 6)]
+
     public void SeedIsGreatherOrEqualThanZero(int seed, float frequency, float amplitude, float lecunarity, float presistance, int octawes)
     {
         var n = new Noise(seed, frequency, amplitude, lecunarity, presistance, octawes);
