3708b25766528a68cb7a340bc682dbb99ed6677e
LeeZer0 <gotikwit@gmail.com>
2019-06-10 22:51:38 +0200

    Task31 Rename

--- ZombieSurvival2D/Assets/Test/Editor/StatsTest.cs
+++ ZombieSurvival2D/Assets/Test/Editor/StatsTest.cs
@@ -1,18 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿
 
-public class NewBehaviourScript : MonoBehaviour
+public class StatsTest 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
