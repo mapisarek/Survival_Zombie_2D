11b6593a1f7de57ad6656d7e88eb6a2ef20b4b72
LeeZer0 <gotikwit@gmail.com>
2019-06-10 23:37:55 +0200

    Task31:add parametr

--- ZombieSurvival2D/Assets/Test/Editor/WeaponTest.cs
+++ ZombieSurvival2D/Assets/Test/Editor/WeaponTest.cs
@@ -8,7 +8,8 @@ public class WeaponTest
     public void WeaponStatsNotMinu(double rel_time, double fast_shot, double mag_cap, double num_mag, 
         double damage, double fast_bulest, double canShot, double canAmo, double amo, double magazin)
     {
-
+        var n = new weapon(rel_time, fast_shot, mag_cap, num_mag
+           damage,  fast_bulest,  canShot,  canAmo,  amo,  magazin);
     }
 
 
