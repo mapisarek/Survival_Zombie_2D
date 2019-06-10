4c9e405159a2d44e1afb6a221212d3e56279a112
LeeZer0 <gotikwit@gmail.com>
2019-06-10 23:34:40 +0200

    Task31;add parametr

--- ZombieSurvival2D/Assets/Test/Editor/WeaponTest.cs
+++ ZombieSurvival2D/Assets/Test/Editor/WeaponTest.cs
@@ -5,8 +5,11 @@ using NUnit.Framework;
 public class WeaponTest
 {
     [Test]
-    public void WeaponStatsNotMinu()
+    public void WeaponStatsNotMinu(double rel_time, double fast_shot, double mag_cap, double num_mag, 
+        double damage, double fast_bulest, double canShot, double canAmo, double amo, double magazin)
     {
+
     }
+
 
 }
