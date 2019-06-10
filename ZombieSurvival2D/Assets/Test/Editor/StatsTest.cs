c1837ef8829edcaceaefe050764adf941907b127
LeeZer0 <gotikwit@gmail.com>
2019-06-10 23:17:23 +0200

    Task31;Remove usles using

--- ZombieSurvival2D/Assets/Test/Editor/StatsTest.cs
+++ ZombieSurvival2D/Assets/Test/Editor/StatsTest.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
+﻿using NUnit.Framework;
+
 
 public class StatsTest 
 {
