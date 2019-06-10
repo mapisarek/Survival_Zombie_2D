f2e34e5b6b245a7cc763609c445a36a484aa64d0
LeeZer0 <gotikwit@gmail.com>
2019-06-10 22:46:32 +0200

    taks 31: Add typ "Stats"

---
+++ ZombieSurvival2D/Assets/Test/Editor/Stats.cs
@@ -0,0 +1,13 @@
+﻿internal class Stats
+{
+    private int maxHealth;
+    private int maxArmour;
+    private int maxStamina;
+
+    public Stats(int maxHealth, int maxArmour, int maxStamina)
+    {
+        this.maxHealth = maxHealth;
+        this.maxArmour = maxArmour;
+        this.maxStamina = maxStamina;
+    }
+}
