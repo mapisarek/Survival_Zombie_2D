85fb2133fbf15f76ed2e8aa7ffc63ac6e775007a
LeeZer0 <gotikwit@gmail.com>
2019-06-10 22:55:31 +0200

    Task31:new type

---
+++ ZombieSurvival2D/Assets/Test/Editor/Stats1.cs
@@ -0,0 +1,13 @@
+﻿internal class Stats1
+{
+    private int currentHealth;
+    private int currentArmour;
+    private int currentStamina;
+
+    public Stats1(int currentHealth, int currentArmour, int currentStamina)
+    {
+        this.currentHealth = currentHealth;
+        this.currentArmour = currentArmour;
+        this.currentStamina = currentStamina;
+    }
+}
