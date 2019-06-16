7b2c58412166d59d7dedc1fbee8e93315324d366
mapisarek <marekpisarek94@gmail.com>
2019-06-16 13:42:04 +0200

    Task 31: HOP MobSpawner refactoring

--- ZombieSurvival2D/Assets/Scripts/MobSpawner.cs
+++ ZombieSurvival2D/Assets/Scripts/MobSpawner.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class MobSpawner : MonoBehaviour
 {
-    public GameObject[] animals;
-    public GameObject[] monsters;
+    private GameObject[] animals;
+    private GameObject[] monsters;
 
     [Space(3)]
     public float waitingForNextSpawn = 10;
     public float theCountdown = 10;
-    public float animalAmount = 15;
+    private float animalAmount = 15;
     public float monstersAmount = 15;
 
     [Header("(X) Spawn Range")]
-    public float xMin;
-    public float xMax;
+    private float xMin;
+    private float xMax;
 
     [Header("(Y) Spawn Range")]
-    public float yMin;
-    public float yMax;
+    private float yMin;
+    private float yMax;
 
     private float animalCount;
     private float monstersCount;
+
+    public float AnimalCount { get => animalCount; set => animalCount = value; }
+    public float AnimalAmount { get => animalAmount; set => animalAmount = value; }
+    public GameObject[] Animals { get => animals; set => animals = value; }
+    public GameObject[] Monsters { get => monsters; set => monsters = value; }
+    public float YMin { get => yMin; set => yMin = value; }
+    public float YMax { get => yMax; set => yMax = value; }
+    public float XMin { get => xMin; set => xMin = value; }
+    public float XMax { get => xMax; set => xMax = value; }
+    public Vector3 Pos { get; set; }
+
 
     public void Update()
     {
@@ -33,28 +44,41 @@ public class MobSpawner : MonoBehaviour
     {
         if (animalCount < animalAmount)
         {
-        theCountdown -= Time.deltaTime;
-        if (theCountdown <= 0)
-        {
-            SpawnAnimals();
-            theCountdown = waitingForNextSpawn;
-        }
+            theCountdown -= Time.deltaTime;
+            if (theCountdown <= 0)
+            {
+                SpawnAnimals();
+                theCountdown = waitingForNextSpawn;
+            }
         }
     }
 
     private void SpawnAnimals()
     {
-        if(animalCount < animalAmount)
+        if (animalCount < animalAmount)
         {
             Vector3 pos = new Vector3(Random.Range(xMin, xMax), Random.Range(yMin, yMax), -1);
             GameObject animalPrefab = animals[Random.Range(0, animals.Length)];
             Instantiate(animalPrefab, pos, Quaternion.identity);
             animalCount++;
         }
-        
+
     }
 
 
 
+}
 
+public class SpawnerGameObjects
+{
+    public void SpawnAnimals(IMobSpawner mobSpawner)
+    {
+        if (mobSpawner.AnimalCount < mobSpawner.AnimalAmount)
+        {
+            mobSpawner.Pos = new Vector3(Random.Range(mobSpawner.XMin, mobSpawner.XMax), Random.Range(mobSpawner.YMin, mobSpawner.YMax), -1);
+            GameObject animalPrefab = mobSpawner.Animals[Random.Range(0, mobSpawner.Animals.Length)];
+            GameObject.Instantiate(animalPrefab, mobSpawner.Pos, Quaternion.identity);
+            mobSpawner.AnimalCount++;
+        }
+    }
 }
---
+++ ZombieSurvival2D/Assets/Test/Editor/SpawnerTest.cs
@@ -0,0 +1,38 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class SpawnerTest
+    {
+
+    [Test]
+    public void IsAnimalSpawning()
+    {
+        var Spawner = Substitute.For<IMobSpawner>();
+        SpawnerGameObjects spawnerObjects = new SpawnerGameObjects();
+        GameObject animal = new GameObject();
+        Spawner.Animals = new GameObject[] { animal };
+        Spawner.AnimalAmount = 10;
+        Spawner.AnimalCount = 10;
+
+        //Maximum amount of animals, block spawning
+        spawnerObjects.SpawnAnimals(Spawner);
+        Assert.AreEqual(Spawner.AnimalCount, Spawner.AnimalAmount);
+
+        //Spawning object from array, count < amount
+        Spawner.AnimalCount = 5;
+        spawnerObjects.SpawnAnimals(Spawner);
+        Assert.AreEqual(6, Spawner.AnimalCount);
+
+        //Spawning not possible, null exception
+        Spawner.Animals = null;
+        Assert.Throws<NullReferenceException>(() => spawnerObjects.SpawnAnimals(Spawner));
+    }
+
+
+}
